7a14b2a242ae7587dac292cf614c8846c8b6d2a6
Mindstormer2000 <caidenkehrer@gmail.com>
2016-08-06 07:35:36 -0600

    commit 2- removed all unneeded files

--- TextBasedGame/Main.cpp
+++ TextBasedGame/Main.cpp
@@ -1,18 +1,3 @@
-// Course: CS 1410 Section 001
-
-// Instructor: Kamali Sarvestani
-
-// File: Lab ##
-
-// Contents: Lab # Programming Assignment
-
-// I declare that the following source code was written
-// solely by me, or provided by the instructor. 
-// I understand that copying any source
-// code, in whole or in part, constitutes cheating, and
-// that I will receive a zero in this assignment if I am
-// found in violation of this policy.
-// -----------------------------------
 
 /*
 Using the following include statements retrive all the
@@ -30,11 +15,7 @@ using namespace std;
 // -----------------------------------------------------------------
 int main()
 {
-	//Output the needed style guide information including my name, course number and lab #
-	cout << "Student name : Caiden Kehrer" << endl << "Course : CS 1410" << endl << "Lab#: 01" << endl;
-	//Create the needed varriables
-
-	//Write the rest of the code past here
+	
 
 	//Pause the system
 	system("Pause");
