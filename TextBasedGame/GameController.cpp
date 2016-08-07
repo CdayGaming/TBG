4cb8ee102f311fbcbdd6deab982c23ea5d1f750c
Mindstormer2000 <caidenkehrer@gmail.com>
2016-08-07 15:19:20 -0600

    Finished game controller to the point I had before

--- TextBasedGame/GameController.cpp
+++ TextBasedGame/GameController.cpp
@@ -1,6 +1,161 @@
 #include "GameController.h"
 
+/*
+ostream & operator<<(ostream & out, Money & clockObj)
+{
+out << "$" << clockObj.GetDollars() << "." << clockObj.GetCents();
+return out;
+}
+*/
 
+//The cout version
+void Out(string _out)
+{
+	string output;
+	for (int i = 0; i < _out.size(); i++)
+	{
+		if (_out[i] == '@')
+		{
+			switch (_out[i + 1])
+			{
+			case 'l':
+				output = output + "\n";
+				i++;
+
+			default:
+				break;
+			}
+
+		}
+		else
+		{
+			output = output + _out[i];
+		}
+	}
+	cout << output << endl;
+}
+
+
+GameController theGame;
+int main()
+{
+	Out("This is a beta mode for caiden kehrers use only ");
+
+	string input;
+	do
+	{
+		switch (theGame.stage)
+		{
+			//Case that they get to the exit screen
+		case 0:
+		{
+			Out("Would you like to quit? (y or n)");
+			cin >> input;
+			try {
+				if (input[0] == 'y' || input[0] == 'Y')
+				{
+					theGame.stage = -1;
+				}
+				else if (input[0] == 'n' || input[0] == 'N')
+				{
+					theGame.stage = 1;
+				}
+				else
+				{
+					throw invalidInput(input);
+				}
+			}
+			catch (out_of_range e)
+			{
+				Out("You did not input anything.");
+
+			}
+			catch (invalidInput e)
+			{
+				Out("You did not input a valid answer");
+				Out(e.invalid + " Was not a valid answer");
+				delay(theGame.theSettings.error_time_delay);
+			}
+		}
+		break;
+		//Case that runs when they are in the settings screen
+		case 1:
+		{
+
+			try
+			{
+				Out("__________Settings__________@l");
+				Out("Edit:@l");
+				Out("1)Error speed@l2)Save and Exit@l3)Back@l");
+				Out("Type the number of the item you want to do/edit : ");
+				cin >> input;
+				int settingInput = 0;
+				//Transfering to an integer value
+				{
+					try
+					{
+						settingInput = stoi(input);
+					}
+					catch (invalid_argument e)
+					{
+						throw invalidInput(input);
+					}
+					catch (out_of_range)
+					{
+						throw invalidInput(input);
+					}
+				}
+				int inputer = -1;
+				switch (settingInput)
+				{
+				case 1:
+				{
+					do {
+						cin.clear();
+						system("cls");
+						Out("Input a number between 0 and 10 for the time to wait in seconds@l Current amount is : " + to_string(theGame.theSettings.error_time_delay));
+						cin >> inputer;
+
+					} while (inputer < 0 || inputer>10);
+
+					theGame.theSettings.error_time_delay = inputer;
+				}
+				break;
+				case 2:
+					theGame.stage = 0;
+					break;
+				default:
+					throw invalidInput(input);
+					break;
+				}
+
+			}
+			catch (out_of_range e)
+			{
+				cout << "You did not input anything." << endl;
+
+			}
+			catch (invalidInput e)
+			{
+				cout << "You did not input a valid answer" << endl;
+				cout << e.invalid << " Was not a valid answer" << endl;
+				delay(theGame.theSettings.error_time_delay);
+			}
+
+
+		}
+		break;
+
+		default:
+			break;
+		}
+	} while (theGame.stage != -1);
+}
+
+void savegame()
+{
+
+}
 
 GameController::GameController()
 {
@@ -8,5 +163,9 @@ GameController::GameController()
 
 
 GameController::~GameController()
+{
+}
+
+void GameController::check()
 {
 }
--- TextBasedGame/GameController.h
+++ TextBasedGame/GameController.h
@@ -1,8 +1,57 @@
 #pragma once
+#include "Player.h"
+#include <string>
+#include <array>
+#include <vector>
+#include "Items.h"
+#include "CustomErrors.h"
+#include <iostream>
+#include <ctime>
+#include <stdio.h>
+#include <time.h>
+#include "Settings.h";
+//This adds in a function for me so I can do delay the console
+#if defined(__WIN32__) || defined(_WIN32) || defined(WIN32) || defined(__WINDOWS__) || defined(__TOS_WIN__)
+
+#include <windows.h>
+
+inline void delay(double seconds)
+{
+
+	Sleep((seconds * 1000));
+}
+
+#else  /* presume POSIX */
+
+#include <unistd.h>
+
+inline void delay(unsigned long ms)
+{
+	usleep(ms * 1000);
+}
+
+#endif 
+//Rest of code past this
+
+
+
+
+using namespace std;
+
 class GameController
 {
+private:
+
+	Player thePlayer;
+	vector<Item> allItem;
+	vector<Weapon> allWeapons;
+	vector<Aurmor> allAurmor;
 public:
+	void Out(string);
+	int stage = 0;
 	GameController();
 	~GameController();
+	void check();
+	Settings theSettings;
 };
 
