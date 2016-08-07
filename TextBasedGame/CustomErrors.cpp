fc62920486fbf6f0591f9a602469da28ad591486
Mindstormer2000 <caidenkehrer@gmail.com>
2016-08-07 10:23:44 -0600

    I know there are errors. I am going to update later to remove them.

--- TextBasedGame/CustomErrors.cpp
+++ TextBasedGame/CustomErrors.cpp
@@ -1,12 +1,32 @@
 #include "CustomErrors.h"
-
-
-
-CustomErrors::CustomErrors()
+/*Inventory overflow area*/
+InventoryOverFlow::InventoryOverFlow()
 {
 }
 
+InventoryOverFlow::~InventoryOverFlow()
+{
+}
+/*No input area*/
+NoInput::NoInput()
+{
+}
+/*Invalid input area*/
+invalidInput::invalidInput(string _input)
+{
+	invalid = _input;
+}
 
-CustomErrors::~CustomErrors()
+
+/*
+Cant insert Item area
+*/
+
+CantInsertItem::CantInsertItem()
+{
+	basicWeapon = Weapon("nothing", 0, 0);
+}
+
+CantInsertItem::~CantInsertItem()
 {
 }
--- TextBasedGame/CustomErrors.h
+++ TextBasedGame/CustomErrors.h
@@ -1,8 +1,36 @@
 #pragma once
-class CustomErrors
+#include <string>;
+#include "Items.h";
+using namespace std;
+
+class InventoryOverFlow
 {
 public:
-	CustomErrors();
-	~CustomErrors();
+	InventoryOverFlow();
+	~InventoryOverFlow();
 };
 
+class NoInput
+{
+public:
+	NoInput();
+};
+
+class invalidInput
+{
+private:
+
+public:
+	string invalid;
+	invalidInput(string _input);
+};
+
+class CantInsertItem
+{
+public:
+	Weapon basicWeapon;
+	Aurmor body, head, legs, feet, gloves;
+	CantInsertItem();
+	~CantInsertItem();
+
+};
